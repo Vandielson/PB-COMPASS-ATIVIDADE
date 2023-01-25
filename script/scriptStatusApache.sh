a7c0b69657bc13f0929999abccb9649ef33f9be3
root <root@ip-172-31-79-159.ec2.internal>
2023-01-25 12:00:32 +0000

    Script para verificar status do Apache

--- script/scriptStatusApache.sh
+++ script/scriptStatusApache.sh
@@ -8,9 +8,9 @@ STATUSAPACHE=$(systemctl status httpd)
 # Condicional para checar se o Apache está ativo ou não e imprimir resultado no arquivo gerado
 if [[ "${STATUSAPACHE}" == *"active (running)"* ]]
 then
-        echo "O Apache está online - "$DATA >> /nfs-atividade/Vandielson/servico_online.txt
+        echo "O Apache está online - "$DATA >> /nfs-atividade/Vandielson/servico_apache_online.txt
 else
  
-        echo "O Apache está offline - "$DATA >> /nfs-atividade/Vandielson/servico_offline.txt
+        echo "O Apache está offline - "$DATA >> /nfs-atividade/Vandielson/servico_apache_offline.txt
 
 fi
